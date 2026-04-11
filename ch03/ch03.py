5efb019f9df8f51dba5b4b58fa1b7a381d0ed76f
Tyler Kieft <tyler.kieft@gmail.com>
2026-04-11 10:24:16 -0400

    Fix loss calculation in LogisticRegressionGD

--- ch03/ch03.py
+++ ch03/ch03.py
@@ -349,7 +349,7 @@ class LogisticRegressionGD:
             errors = (y - output)
             self.w_ += self.eta * X.T.dot(errors) / X.shape[0]
             self.b_ += self.eta * errors.mean()
-            loss = -y.dot(np.log(output)) - ((1 - y).dot(np.log(1 - output))) / X.shape[0]
+            loss = (-y.dot(np.log(output)) - (1 - y).dot(np.log(1 - output))) / X.shape[0]
             self.losses_.append(loss)
         return self
 
